0e0cebe0c4723db9a9d8346c73d9e9b2953856de
qwp0905 <qwp0905@gmail.com>
2025-12-05 09:15:17 +0900

    fix typo

--- src/node.ts
+++ src/node.ts
@@ -34,7 +34,8 @@ class HandlerNode {
     this.pattern = this.pattern.slice(match.length)
     this.failure &&= null
     const node = new HandlerNode(match)
-    node.children = new Map([[this.pattern[0], this]])
+    node.children = new Map()
+    node.children.set(this.pattern[0], this)
     return node
   }
 
